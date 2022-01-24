051ae21389b10ee95442912f078544f5d70f5bfb
SergioCano <sergio0201david@gmail.com>
2022-01-24 07:23:05 -0500

    cambios

--- src/components/documentMaster/DocumentMasterView.jsx
+++ src/components/documentMaster/DocumentMasterView.jsx
@@ -178,7 +178,7 @@ export const DocumentMasterView = () => {
             img:
               DocumentMasterBody.image === null ? "" : DocumentMasterBody.image,
             heigth: { state: true },
-            titleCard: DocumentMasterBody.title_card,
+            titleCard: '',
             optionValue: DocumentMasterBody.select_value,
             text:
               DocumentMasterBody.text_description === null
